27541bbd5ce69e4f2e7c10ba28c323bbdfb3b293
luricisme <trinhnguyenluong182306@gmail.com>
2025-12-26 18:58:25 +0700

    First commit

---
+++ src/main/java/group/five/Website_PetCareX/WebsitePetCareXApplication.java
@@ -0,0 +1,13 @@
+package group.five.Website_PetCareX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitePetCareXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsitePetCareXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/five/Website_PetCareX/WebsitePetCareXApplicationTests.java
@@ -0,0 +1,13 @@
+package group.five.Website_PetCareX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitePetCareXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
